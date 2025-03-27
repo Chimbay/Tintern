1b4007857cbcec4b212b18c22c66cbd629bc1d28
Chimbay <c.anthony.priv@gmail.com>
2025-03-26 20:19:37 -0500

    Adding display

---
+++ src/components/dock/main.jsx
@@ -0,0 +1,11 @@
+import style from "./main.module.css"
+
+export default function dock() {
+    return (
+        <div className={style.dock}>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
